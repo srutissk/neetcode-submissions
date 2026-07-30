d9d873804a0543c2f59d75be1f1c21c61a9a965c
Sruti <srutisk11@gmail.com>
2026-07-30 00:09:10 -0400

    Add: merge-intervals - submission-0

---
+++ Data Structures & Algorithms/merge-intervals/submission-0.py
@@ -0,0 +1,20 @@
+class Solution:
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        # sweep line (2)?
+        mp = defaultdict(int)
+        for start, end in intervals:
+            mp[start] += 1
+            mp[end] -= 1
+        
+        res = []
+        interval = []
+        have = 0
+        for i in sorted(mp):
+            if not interval:
+                interval.append(i)
+            have += mp[i]
+            if have == 0:
+                interval.append(i)
+                res.append(interval)
+                interval = []
+        return res
